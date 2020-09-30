19e410aea78479c109ceb8582ff9a706ae8eead4
Massu <imomushi.pipi@gmail.com>
2020-10-01 01:22:59 +0900

    [add] Main constructor

---
+++ main.py
@@ -0,0 +1,30 @@
+import argparse
+import threading
+import time
+import traceback
+
+from odpt import ODPT
+from ledctrl import LEDCtrl
+
+
+class Main():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("-s", "--service", action="store",
+                                 help="使用するサービス. Default: metro", default="metro", type=str, choices=["odpt", "metro"])
+        self.parser.add_argument("-l", "--lines", action="store",
+                                 help="表示する路線(独立制御は2本まで). Default: G", default=["G"], type=str, nargs='*')
+        self.args = self.parser.parse_args()
+
+        service = self.args.service
+        self.lines = self.args.lines
+
+        self.odpt = ODPT(service)
+        stations = self.odpt.get_stationtable()
+
+        self.led = LEDCtrl(stations)
+        self.led.strip_setup(self.lines)
+
+
+if __name__ == "__main__":
+    main = Main()
