fc9fdb2a6425be249b55ba44a693a368171e1448
Massu <imomushi.pipi@gmail.com>
2020-10-03 14:22:27 +0900

    [change] Integrated services
    odptとmetroを統合し，路線指定のみで動作するように

--- main.py
+++ main.py
@@ -10,16 +10,13 @@ from ledctrl import LEDCtrl
 class Main():
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("-s", "--service", action="store",
-                                 help="使用するサービス. Default: metro", default="metro", type=str, choices=["odpt", "metro"])
         self.parser.add_argument("-l", "--lines", action="store",
                                  help="表示する路線(独立制御は2本まで). Default: G", default=["G"], type=str, nargs='*')
         self.args = self.parser.parse_args()
 
-        service = self.args.service
         self.lines = self.args.lines
 
-        self.odpt = ODPT(service)
+        self.odpt = ODPT()
         stations = self.odpt.get_stationtable()
 
         self.led = LEDCtrl(stations)
--- odpt.py
+++ odpt.py
@@ -7,42 +7,57 @@ class ODPT():
 
     Parameters
     ----------
-    service : str
-        オープンデータのサービス("odpt" or "metro")
     jsonpath : str
         api_config.jsonのパス
 
     Attributes
     ----------
-    service : str
-        オープンデータのサービス
     update_freq : int
         APIデータの更新間隔
     '''
 
-    def __init__(self, service, jsonpath="./config/api_config.json"):
-        '''
-        '''
+    def __init__(self, jsonpath="./config/api_config.json"):
+
         with open(jsonpath, 'r') as cf:
             config = json.load(cf)
 
-        self.service = service
         self.update_freq = config["update_freq"]
 
-        self.__key = config[service]["token"]
-        self.__odpt_tra = config[service]["Train"]
-        self.__odpt_rwy = config[service]["Railway"] + self.__key
+        self.__odpt_key = config["odpt"]["token"]
+        self.__metro_key = config["metro"]["token"]
 
-    def get_railway(self):
+        for k in config["odpt"]["Train"].keys():
+            config["odpt"]["Train"][k] += self.__odpt_key
+
+        for k in config["metro"]["Train"].keys():
+            config["metro"]["Train"][k] += self.__metro_key
+
+        self.__trains = {}
+        self.__trains.update(**config["odpt"]["Train"], **config["metro"]["Train"])
+
+        self.__odpt_rwy = config["odpt"]["Railway"] + self.__odpt_key
+        self.__metro_rwy = config["metro"]["Railway"] + self.__metro_key
+
+    def get_railway(self, service):
         '''"service"で提供される路線情報を取得する
+
+        Parameters
+        ----------
+        service : str
+            オープンデータのサービス("odpt" or "metro")
 
         Returns
         -------
         data : list of dict
             路線ごとの駅情報
         '''
+        if service == "odpt":
+            r = requests.get(self.__odpt_rwy)
+        elif service == "metro":
+            r = requests.get(self.__metro_rwy)
+        else:
+            return None
 
-        r = requests.get(self.__odpt_rwy)
         railway_data = r.json()
 
         return railway_data
@@ -61,36 +76,41 @@ class ODPT():
             指定した路線の列車走行位置情報
         '''
 
-        r = requests.get(self.__odpt_tra[line] + self.__key)
+        r = requests.get(self.__trains[line])
         train_data = r.json()
         
         return train_data
 
 
     def get_stationtable(self):
-        '''静的ファイルから"service"の駅テーブルを取得する
+        '''静的ファイルから駅テーブルを取得する
 
         Returns
         -------
         sta_table : dict
-            "service"の路線ごとの駅テーブル
+            路線ごとの駅テーブル
         '''
-        jsonpath = f"./data/{self.service}_stations.json"
+        jsonpath = f"./data/station_table.json"
 
         with open(jsonpath, 'r') as sta:
             sta_table = json.load(sta)
 
         return sta_table
 
-    def get_stationtable_api(self):
+    def get_stationtable_api(self, service):
         '''APIから"service"の駅テーブルを取得する
+
+        Parameters
+        ----------
+        service : str
+            オープンデータのサービス("odpt" or "metro")
 
         Returns
         -------
         sta_table : dict
             "service"の路線ごとの駅テーブル
         '''
-        railway = self.get_railway()
+        railway = self.get_railway(service)
         sta_table = {}
 
         for i in range(len(railway)):
@@ -98,7 +118,7 @@ class ODPT():
             sta_table[railway[i]["odpt:lineCode"]] = {}
 
             for j in range(len(sta_order)):
-                if self.service == "metro":
+                if service == "metro":
                     sta_table[railway[i]["odpt:lineCode"]].update([(sta_order[j]["odpt:station"], sta_order[j]["odpt:index"])])
                 else:
                     sta_table[railway[i]["odpt:lineCode"]].update([(sta_order[j]["odpt:station"], sta_order[j]["odpt:index"] - 1)])
